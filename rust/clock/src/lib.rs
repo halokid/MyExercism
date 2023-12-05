b10c496154350707b844101973e48f385249f1df
mm jxx <r00txxyyzz@gmail.com>
2023-12-05 23:07:32 +0800

    done clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,8 +1,8 @@
 use std::fmt;
 use std::fmt::Formatter;
 
-fn modulo(total_min: i32, day_min: i32) -> i32 {
-    (total_min % day_min + day_min ) % day_min
+fn total_to_mins(total_mins: i32, day_mins: i32) -> i32 {
+    (total_mins % day_mins + day_mins ) % day_mins
 }
 
 #[derive(Debug, PartialEq)]
@@ -12,17 +12,26 @@ pub struct Clock {
 }
 
 impl fmt::Display for Clock {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}{:02}", self.hours, self.mins)
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.mins)
     }
 }
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!()
+    pub fn new(input_hours: i32, input_minutes: i32) -> Clock {
+        let total_mins = input_hours * 60 + input_minutes;
+        let day_mins = 60 * 24;
+        let count_mins = total_to_mins(total_mins, day_mins);
+
+        let hours = count_mins / 60;
+        let mins = count_mins % 60;
+        Clock{ hours, mins }
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+    pub fn add_minutes(&self, minutes: i32) -> Clock {
+        Clock::new(self.hours, self.mins + minutes)
     }
 }
+
+
+
