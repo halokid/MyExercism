db47147821f5d351eda154a265096658dd7eacde
mm jxx <r00txxyyzz@gmail.com>
2023-12-21 23:35:57 +0800

    done sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -9,40 +9,34 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-  let first_len = _first_list.len();
-  let second_len = _second_list.len();
+  if _first_list == _second_list {
+    return Comparison::Equal;
+  }
 
-  if first_len == 0 && second_len != 0 {
+  if _first_list.len() == 0 && _second_list.len() > 0 {
     return Comparison::Sublist;
   }
 
-  if first_len != 0 && second_len == 0 {
+  if _second_list.len() == 0 && _first_list.len() > 0 {
     return Comparison::Superlist;
   }
 
-  if first_len <= second_len {
-    let start_position = find_position(_first_list.get(0), _second_list);
-
-    if (start_position + first_len > second_len) || start_position == 0 {
-      return Comparison::Unequal;
-    }
-
-    // if _first_list == _second_list[start_position..second_len]
-  }
-
-  Comparison::Sublist
-}
-
-// find start position for item in collection
-fn find_position<T: PartialEq>(item: T, list: &[T]) -> usize {
-  for i in 0..list.len() {
-    if list[i] == item {
-      return i;
+  if _first_list.len() > _second_list.len() {
+    if _first_list.windows(_second_list.len())
+      .any(|items| items == _second_list) {
+      return  Comparison::Superlist;
     }
   }
-  return 0;
-}
 
+  if _second_list.len() > _first_list.len() {
+    if _second_list.windows(_first_list.len())
+      .any(|items| items == _first_list) {
+      return  Comparison::Sublist;
+    }
+  }
+
+  return  Comparison::Unequal;
+}
 
 
 
--- rust/sublist/src/main.rs
+++ rust/sublist/src/main.rs
@@ -4,5 +4,6 @@ fn main() {
   }
 
   let list = &[1, 2, 3, 4, 5, 6, 7, 8, 9];
-  println!("{}", list[2 : 5]);
+  // println!("{}", list[2 : 5]);
+  println!("{}", list[2]);
 }
