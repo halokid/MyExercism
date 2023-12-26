254d1afce14e04e9b7a56bca7d53cfc8919162be
Hays <Hays>
2023-12-26 18:40:26 +0800

    minesweeper

---
+++ rust/minesweeper/src/lib.rs
@@ -0,0 +1,14 @@
+
+const MINE_BYTE: u8 = b'*';
+
+fn count_mines(minefield: &[&str], x: usize, y: usize) -> usize {
+    !todo!()
+    // (y.saturating_sub(1)..= y + 1)
+    //   .find_map()
+}
+
+pub fn annotate(minefield: &[&str]) -> Vec<String> {
+    todo!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:#?}\n");
+}
+
+
---
+++ rust/minesweeper/src/main.rs
@@ -0,0 +1,73 @@
+fn main() {
+  let s = &["he", "lo"];
+  for (row) in s.iter() {
+    // println!("i: {}, row: {:?}", i, row);
+    println!(" row: {:?}", row);
+  }
+
+  // let minefield = &[
+  let test_case = &[
+    "111",
+    "1*1",
+    "111",
+  ];
+
+  // for (i, row) in minefield.iter() {
+  //   println!("i: {}, row: {:?}", i, row);
+  // }
+
+  let cleaned = remove_annotations(test_case);
+  let cleaned_strs = cleaned.iter().map(|r| &r[..]).collect::<Vec<_>>();
+  let expected = test_case.iter().map(|&r| r.to_string()).collect::<Vec<_>>();
+
+  // for (row_line_number, row) in minefield.iter().enumerate() {
+  for (row_line_number, row) in &cleaned_strs.iter().enumerate() {
+    println!("===============================================================");
+    println!("row_line_number: {}, row: {}", row_line_number, row);
+    for (row_item_number, item) in row.as_bytes().iter().enumerate() {
+      println!("------------------------------------------");
+      println!("row_item_number: {}, item: {}", row_item_number, item);
+      // test_count(minefield, row_item_number, row_line_number);
+      test_count(&cleaned_strs, row_item_number, row_line_number);
+    }
+  }
+}
+
+fn test_count(minefield: &[&str], row_item_number: usize, row_line_number: usize) -> usize {
+  // !todo!()
+  let line_res = (row_line_number.saturating_sub(1)..=row_line_number + 1)
+    // .filter_map(|y|println!("minefield y -->>> {}", minefield.get(row_line_number)));
+    // .filter_map(|y|println!(minefield.get(row_line_number)))
+    .filter_map(|y| minefield.get(row_line_number))
+    .flat_map(|line| (row_item_number.saturating_sub(1)..=row_item_number + 1).filter_map(move |x| line.as_bytes().get(x)))
+    .filter(|&&c| c == b'*');
+  // .count();
+  println!("line_res -->>> {:?}", line_res.count());
+
+  return 0;
+}
+
+fn remove_annotations(board: &[&str]) -> Vec<String> {
+    board.iter().map(|r| remove_annotations_in_row(r)).collect()
+}
+fn remove_annotations_in_row(row: &str) -> String {
+    row.chars()
+        .map(|ch| match ch {
+            '*' => '*',
+            _ => ' ',
+        })
+        .collect()
+}
+
+fn run_test(test_case: &[&str]) {
+  // let cleaned = remove_annotations(test_case);
+  // let cleaned_strs = cleaned.iter().map(|r| &r[..]).collect::<Vec<_>>();
+  // let expected = test_case.iter().map(|&r| r.to_string()).collect::<Vec<_>>();
+  // assert_eq!(expected, annotate(&cleaned_strs));
+}
+
+
+
+
+
+
