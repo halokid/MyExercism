3fffc4ded3b180e14093061ba1d237934466eccc
mm jxx <r00txxyyzz@gmail.com>
2023-12-29 00:38:43 +0800

    coding minesweeper

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,14 +1,23 @@
+const MINE: u8 = b'*';
 
-const MINE_BYTE: u8 = b'*';
+fn count_mines(minefield: &[&str], row_item_number: usize, row_line_number: usize) -> usize {
+  let count = (row_line_number.saturating_sub(1)..= row_line_number + 1)
+    .filter_map(|row_line_number| minefield.get(row_line_number))
+    .flat_map(|line| (row_item_number.saturating_sub(1)..= row_item_number + 1).filter_map(move |x| line.as_bytes().get(x)))
+    .filter(|&&c| c == MINE)
+    .count();
 
-fn count_mines(minefield: &[&str], x: usize, y: usize) -> usize {
-    !todo!()
-    // (y.saturating_sub(1)..= y + 1)
-    //   .find_map()
+  count
 }
 
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    todo!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:#?}\n");
+  let mut res = vec![];
+
+  for (row_line_number, row) in minefield.iter().enumerate() {
+
+  }
+
+  ()
 }
 
 
